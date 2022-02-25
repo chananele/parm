f70e93708d2fcb7c24c6cff55bee2ff09e92af73
Chananel Engelberg <chananelyisrael@gmail.com>
2022-02-25 19:09:09 +0200

    Add postlex logic to require whitespace after opcode

--- parm/parser.py
+++ parm/parser.py
@@ -1,11 +1,24 @@
 from lark import Lark
 
+from postlex import ArmSpacer
+
+
+class Spacer(ArmSpacer):
+    ws_type = '_WS'
+    injected_type = '_POST_OPCODE'
+
+    def is_opcode_types(self, token_type):
+        return token_type.startswith('OPCODE')
+
 
 def load_parser():
-    parser = Lark.open('arm.lark', rel_to=__file__, parser='lalr')
+    spacer = Spacer()
+    parser = Lark.open('arm.lark', rel_to=__file__, parser='lalr', postlex=spacer)
     print(parser.parse('0x1000: ldm r0, {r0, r2-r5}'))
     print(parser.parse('strbeq r2, [r4, lsl#8]'))
     print(parser.parse('LDR R8, [R0, R0, ASR#31]'))
+    print(parser.parse('25: blx lr'))
+    print(parser.parse('25: beq 0x2000'))
 
 
 def main():
---
+++ parm/postlex.py
@@ -0,0 +1,43 @@
+from abc import ABC, abstractmethod
+from lark.lark import PostLex
+from lark.lexer import Token
+
+
+class ArmSpacer(PostLex, ABC):
+    def process(self, stream):
+
+        opcode = None
+        previous_token = None
+
+        for token in stream:
+
+            if self.is_opcode_types(token.type):
+                assert opcode is None
+                opcode = token
+
+            if token.type == self.ws_type:
+                if opcode is not None and previous_token is opcode:
+                    yield Token(self.injected_type, '')
+            else:
+                yield token
+
+            previous_token = token
+
+    @abstractmethod
+    def is_opcode_types(self, token_type):
+        raise NotImplementedError()
+
+    @property
+    @abstractmethod
+    def ws_type(self):
+        raise NotImplementedError()
+
+    @property
+    @abstractmethod
+    def injected_type(self):
+        raise NotImplementedError()
+
+    @property
+    def always_accept(self):
+        result = (self.ws_type, )
+        return result
