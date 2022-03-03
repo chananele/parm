ae83b4dff947980b2eb742cf0832bc245a7591eb
Chananel Engelberg <chananelyisrael@gmail.com>
2022-03-03 17:28:58 +0200

    Remove unused postlex logic

--- parm/parsers.py
+++ parm/parsers.py
@@ -1,34 +1,5 @@
 from lark import Lark
 
-from parm.postlex import Spacer, PatternSpacer
-
-
-class ArmSpacer(Spacer):
-    ws_type = '_WS'
-    injected_type = '_POST_OPCODE'
-
-    def is_opcode_type(self, token_type):
-        return token_type.startswith('OPCODE')
-
-    def is_operand_type(self, token_type):
-        return not self.is_opcode_type(token_type)
-
-
-class ArmPatSpacer(PatternSpacer):
-    ws_type = '_WS'
-    injected_type = '_POST_OPCODE'
-    label_type = 'CAPTURE'
-
-    def is_opcode_type(self, token_type):
-        return token_type.startswith('OPCODE')
-
-    def is_operand_type(self, token_type):
-        if not self.is_opcode_type(token_type):
-            return True
-        if 'WILDCARD' in token_type:
-            return True
-        return False
-
 
 def create_parser(path, rel_to, start, postlex=None, parser='earley', lexer=None):
     if parser == 'earley' and lexer is None:
