b57edc7d6082de328913813e50c1de7ffe39309d
Ido Kuks <idokuks@google.com>
2022-03-21 14:02:30 +0200

    env: capstone: Implement simple binary disassemble using capstone

--- parm/envs/capstone.py
+++ parm/envs/capstone.py
@@ -1,7 +1,15 @@
-from typing import Iterator
+from pathlib import Path
+from typing import IO, Iterator, Union
 
 from parm.api.asm_cursor import AsmCursor
 from parm.programs.snippet import ArmSnippetProgram
+
+from capstone import CS_ARCH_ARM, CS_ARCH_X86, CS_MODE_ARM, Cs, CsError
+
+CAPSTONE_ARCH = {
+    "arm": CS_ARCH_ARM,
+    "x86": CS_ARCH_X86,  # Not supported yet
+}
 
 
 class CapstoneProgram(ArmSnippetProgram):
@@ -33,6 +41,35 @@ class CapstoneProgram(ArmSnippetProgram):
 
     def add_env_fixtures(self):
         self.env.inject_magic_getter('xrefs_to', self.get_xrefs_to)
+
+
+def disassemble_binary_file(
+        binary_path: Path,
+        arch: str,
+        mode: int,
+        offset: int = 0,
+        size: Union[int, None] = None) -> Union[bytes, bool]:
+    with binary_path.open('rb') as bf:
+        binary_file.seek(offset)
+        ops = binary_file.read(size)
+
+    if not ops:
+        print("Nothing to disassemble ?")
+        return False
+
+    # TODO: get capstone mode according to arch-mode
+    try:
+        md = Cs(CAPSTONE_ARCH[arch], CS_MODE_ARM)
+        if not (dis := md.disasm(ops, offset)):
+            return False
+        return "\n".join(f"0x{inst.address:x}: {inst.mnemonic} {inst.op_str}"
+                         for inst in dis)
+    except CsError as e:
+        print(
+            f"Failed initiating capstone for arch: {(CAPSTONE_ARCH[arch], CS_MODE_ARM)} with: {str(e)}"
+        )
+        return False
+
 
 def capstone_create_program(code: str) -> CapstoneProgram:
     return CapstoneProgram(code)
