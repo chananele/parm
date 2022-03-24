d01cd2f27d978087a5d31c5006c49b56cfe76f42
Chananel Engelberg <chananelyisrael@gmail.com>
2022-03-24 19:15:09 +0200

    Add basic fixes so the sample capstone test passes

--- parm/tests/capstone_program_test.py
+++ parm/tests/capstone_program_test.py
@@ -2,19 +2,29 @@ from pathlib import Path
 from unittest import TestCase
 
 from parm.api.match_result import MatchResult
-from parm.api.parsing.arm_asm import *
 from parm.envs.capstone import capstone_create_program_from_file
 
 SNIPPETS_DIR = Path(__file__).parent / "snippets"
 
 
+def _is_excluded_file(f: Path):
+    if f.suffix in ('.id0', '.id1', '.id2', '.nam', '.til'):
+        return True
+    return False
+
+
+def _load_programs():
+    return {
+        snippet.name: capstone_create_program_from_file(snippet)
+        for snippet in SNIPPETS_DIR.iterdir()
+        if not _is_excluded_file(snippet)
+    }
+
+
 class CapstoneProgramTest(TestCase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.programs = {
-            snippet.name: capstone_create_program_from_file(snippet)
-            for snippet in SNIPPETS_DIR.iterdir()
-        }
+        self.programs = _load_programs()
 
     def test_find_single(self):
         prg = self.programs['mul']
